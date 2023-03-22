01e70258b4f795d8155853554019001c90bb2d90
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-22 22:47:19 +0000

    Added VDP codes

--- agon.h
+++ agon.h
@@ -2,12 +2,13 @@
 // Title:	        Agon Video BIOS - Function prototypes
 // Author:        	Dean Belfield
 // Created:       	05/09/2022
-// Last Updated:    21/03/2023
+// Last Updated:    22/03/2023
 //
 // Modinfo:
 // 04/03/2023:      Added LOGICAL_SCRW and LOGICAL_SCRH
 // 17/03/2023:		Added PACKET_RTC, EPOCH_YEAR, MAX_SPRITES, MAX_BITMAPS
 // 21/03/2023:		Added PACKET_KEYSTATE
+// 22/03/2023:		Added VDP codes
 
 #pragma once
 
@@ -31,15 +32,32 @@
 
 #define GPIO_ITRP				17		// VSync Interrupt Pin - for reference only
 
-#define PACKET_GP				0		// General poll data
-#define PACKET_KEYCODE			1		// Keyboard data
-#define PACKET_CURSOR			2		// Cursor positions
-#define PACKET_SCRCHAR			3		// Character read from screen
-#define PACKET_SCRPIXEL			4		// Pixel read from screen
-#define PACKET_AUDIO			5		// Audio acknowledgement
-#define PACKET_MODE				6		// Get screen dimensions
-#define PACKET_RTC				7		// RTC
-#define PACKET_KEYSTATE			8		// Keyboard repeat rate and LED status
+// Commands for VDU 23, 0, n
+//
+#define VDP_GP					0x80	// General poll data
+#define VDP_KEYCODE				0x81	// Keyboard data
+#define VDP_CURSOR				0x82	// Cursor positions
+#define VDP_SCRCHAR				0x83	// Character read from screen
+#define VDP_SCRPIXEL			0x84	// Pixel read from screen
+#define VDP_AUDIO				0x85	// Audio acknowledgement
+#define VDP_MODE				0x86	// Get screen dimensions
+#define VDP_RTC					0x87	// RTC
+#define VDP_KEYSTATE			0x88	// Keyboard repeat rate and LED status
+#define VDP_LOGICALCOORDS		0xC0	// Switch BBC Micro style logical coords on and off
+#define VDP_TERMINALMODE		0xFF	// Switch to terminal mode
+
+// And the corresponding return packets
+// By convention, these match their VDP counterpart, but with the top bit reset
+//
+#define PACKET_GP				0x00	// General poll data
+#define PACKET_KEYCODE			0x01	// Keyboard data
+#define PACKET_CURSOR			0x02	// Cursor positions
+#define PACKET_SCRCHAR			0x03	// Character read from screen
+#define PACKET_SCRPIXEL			0x04	// Pixel read from screen
+#define PACKET_AUDIO			0x05	// Audio acknowledgement
+#define PACKET_MODE				0x06	// Get screen dimensions
+#define PACKET_RTC				0x07	// RTC
+#define PACKET_KEYSTATE			0x08	// Keyboard repeat rate and LED status
 
 #define AUDIO_CHANNELS			3		// Number of audio channels
 #define PLAY_SOUND_PRIORITY 	3		// Sound driver task priority with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest
