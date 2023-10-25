fa186d60f7cb663bd54bb807edc412c8cce5a137
Steve Sims <steve@stevesims.dev>
2023-10-25 16:43:00 +0100

    Semantic versioning
    move version info out to `version.h` file
    
    version info now a semantic version, with major, minor, patch, and “candidate” info, as well as an indicator for candidate type (displayed if candidate number is non-zero)
    
    support for showing an automatically-generated build number added, which can be provided by a build script and is intended to be derived from the git hash
    
    with this approach it should also be possible in the future to add other tooling to automatically increment version numbers
    
    version info also includes the codebase variant, allowing us to easily differentiate between Quark and Console8

---
+++ video/version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define		VERSION_MAJOR		2
+#define		VERSION_MINOR		0
+#define		VERSION_PATCH		0
+#define		VERSION_CANDIDATE	1			// Optional
+#define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
+
+#define		VERSION_VARIANT		"Quark"
+
+#endif // VERSION_H
